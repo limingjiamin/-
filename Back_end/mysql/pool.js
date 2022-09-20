a0d3af8ba59f8d1a3335b29abeb9d20e079b54ad
lishuangling <595237147@qq.com>
2022-09-20 19:55:56 +0800

    请求数据，修改样式

--- Back_end/mysql/pool.js
+++ Back_end/mysql/pool.js
@@ -4,8 +4,8 @@ let pool = mysql.createPool({
   host: "localhost",
   post: 3306,
   user: "root",
-  password: "123456",
-  database: "management",
+  password: "root",
+  database: "mall",
   connectionLimit: 10,
 });
 
